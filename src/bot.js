543ce4d534d41476469bd0d686cf77090d983c01
kjscott27 <kyle.j.scott@hotmail.com>
2018-12-01 13:30:20 +0000

    add eslint, flow & babel

--- src/bot.js
+++ src/bot.js
@@ -1,3 +1,4 @@
+//@flow
 const Discord = require('discord.io');
 const logger = require('winston');
 const auth = require('../auth.json');
@@ -17,25 +18,16 @@ const bot = new Discord.Client({
 
 bot.on('ready', () => {
   logger.info('Connected');
-  logger.info('Logged in as: ');
-  logger.info(bot.username + ' - (' + bot.id + ')');
-  bot.user.setPresence({game: {
-    name: 'With Javascript',
+  logger.info(`Logged in as: ${bot.username}(${bot.id})!`);
+  bot.setPresence({game: {
+    name: 'with JavaScript',
     type: 0
   }})
 });
 
-bot.on('message', (receivedMessage) => {
-  // prevent the bot from responding to its own messages
-  if (receivedMessage === bot.user) {
-    return;
-  }
-
-  receivedMessage.react("👍");
-});
-
-
 bot.on('message', (user, userID, channelID, message) => {
+
+  const filter = (reaction) => reaction.emoji.name === '👌' && user.id === user;
 
   // Commands are executed using ! -- bot will only listen and check for messages starting with !.
   if (message.substring(0, 1) === '!') {
@@ -58,15 +50,16 @@ bot.on('message', (user, userID, channelID, message) => {
       case 'help':
         bot.sendMessage({
           to: channelID,
-          message: 'Current Commands: !ping, !help'
-        });
+          message: 'Current Commands: !ping, !help' })
+          .then(message.awaitReactions(filter, { time: 1500 }))
+          .then(message.delete)
+          .catch(console.error);
         break;
       default:
         bot.sendMessage({
           to: channelID,
           message: user + ' - Sorry I dont know that one, please type !help for a full list of commands'
         })
-      // Just add any case commands if you want to..
     }
   }
 });
