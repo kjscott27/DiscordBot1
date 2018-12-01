3dd824e9842d45ec2cc77f1829416949aec8b86b
kjscott27 <kyle.j.scott@hotmail.com>
2018-12-01 11:03:30 +0000

    initial discord bot commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## Description
+First attempt at making a discord bot, not sure what it'll do yet but its purely for learning purposes at the moment.
+
+### Helpful Commands (Windows)
+- taskkill /F /IM node.exe (finds and kills all node.exe tasks)
+- tasklist (displays all tasks running)
+
+### Helpful Commands (Mac)
+- killall node (finds and kills all node.exe tasks)
---
+++ bot.js
@@ -0,0 +1,53 @@
+const Discord = require('discord.io');
+const logger = require('winston');
+const auth = require('./auth.json');
+
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {
+  colorize: true
+});
+logger.level = 'debug';
+
+// Initialize Discord Bot
+const bot = new Discord.Client({
+  token: auth.token,
+  autorun: true
+});
+bot.on('ready', function (evt) {
+  logger.info('Connected');
+  logger.info('Logged in as: ');
+  logger.info(bot.username + ' - (' + bot.id + ')');
+});
+bot.on('message', function (user, userID, channelID, message, evt) {
+
+  // Our bot needs to know if it will execute a command
+  // It will listen for messages that will start with `!`
+  if (message.substring(0, 1) === '!') {
+    let args = message.substring(1).split(' ');
+    let cmd = args[0];
+
+    args = args.splice(1);
+    switch(cmd) {
+      // !ping
+      case 'ping':
+        bot.sendMessage({
+          to: channelID,
+          message: 'Pong!'
+        });
+        break;
+      case 'help':
+        bot.sendMessage({
+          to: channelID,
+          message: '!ping, !help'
+        });
+        break;
+      default:
+        bot.sendMessage({
+          to: channelID,
+          message: user + '- Sorry I dont know that one, please type !help for a full list of commands'
+        })
+      // Just add any case commands if you want to..
+    }
+  }
+});
