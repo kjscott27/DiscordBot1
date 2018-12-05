2654e497c1e2344c1aea27c671b13cc86cc8948d
kjscott27 <kyle.j.scott@hotmail.com>
2018-12-05 13:59:01 +0000

    Complete rewrite to use discord.js instead

--- src/bot.js
+++ src/bot.js
@@ -1,63 +1,35 @@
 // @flow
-const Discord = require('discord.io');
+const Discord = require('discord.js');
 const logger = require('winston');
-const auth = require('../auth.json');
+const token = require('../token.js');
 
-// Configure logger settings
+const prefix = 'tb!';
+
+// Create the bot as a new Discord Client
+const bot = new Discord.Client();
+
+// Configure Logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console(), {
   colorize: true,
 });
 logger.level = 'debug';
 
-// Initialize Discord Bot
-const bot = new Discord.Client({
-  token: auth.token,
-  autorun: true,
-});
-
+// Initialize the Bot
 bot.on('ready', () => {
-  logger.info('Connected');
-  logger.info(`Logged in as: ${bot.username}(${bot.id})!`);
-  bot.setPresence({
-    game: {
-      name: 'with JavaScript',
-      type: 0,
-    },
-  });
+  logger.info('Ready!');
+  bot.user.setActivity('Playing with JavaScript', 'twitch.tv/kjs0');
 });
 
-bot.on('message', (user, userID, channelID, message) => {
-  // Commands are executed using ! -- bot will only listen and check for messages starting with !.
-  if (message.substring(0, 1) === '!') {
-    const args = message.substring(1).split('');
-    const cmd = args[0];
+bot.on('message', (message) => {
+  if (message.author.bot) {
+    return;
+  }
 
-    switch (cmd) {
-      case 'intro':
-        bot.sendMessage({
-          to: channelID,
-          message:
-            'I am a bot being used to study and test javascript skills. Type !help for a list of commands.',
-        });
-        break;
-      case 'ping':
-        bot.sendMessage({
-          to: channelID,
-          message: 'Pong!',
-        });
-        break;
-      case 'help':
-        bot.sendMessage({
-          to: channelID,
-          message: 'Current Commands: !ping, !help',
-        });
-        break;
-      default:
-        bot.sendMessage({
-          to: channelID,
-          message: `${user} - Sorry I dont know that one, please type !help for a full list of commands`,
-        });
-    }
+  if (message.content.startsWith(`${prefix}hello`)) {
+    message.reply('Hi!');
   }
 });
+
+// Log the bot into Discord (this always should be the final command)
+bot.login(token.DISCORD_BOT_TOKEN);
